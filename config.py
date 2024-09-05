48450b2f3a7b09f3c7b9cc2d7f97a62c2eeadd0f
kanet2 <kanet.ochoa1921@alumnos.udg.mx>
2024-09-05 11:47:17 -0600

    P6 sql

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    SECRET_GEY  = 'sabadaBADABUDFJLSDFSHGQHGOIHhohihoiHOIFH'
+    DEBUG       = True
+
+class DevelopmentConfig:
+    MYSQL_HOST      = 'localhost'
+    MYSQL_USER      = 'root'
+    MYSQL_PASSWORD  = ''
+    MYSQL_DB        = 'spendy'
+
+
+    
