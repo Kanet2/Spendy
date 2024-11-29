606977468ffcf863535d7720c92b93f9d96a2eb4
kanet2 <kanet.ochoa1921@alumnos.udg.mx>
2024-11-28 22:06:21 -0600

    p.19 base de datos

--- static/sql/spendy.sql
+++ static/sql/spendy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 14-11-2024 a las 06:10:50
+-- Tiempo de generación: 29-11-2024 a las 04:05:26
 -- Versión del servidor: 8.0.40
 -- Versión de PHP: 8.1.30
 
@@ -31,10 +31,18 @@ CREATE TABLE `inversiones` (
   `id` int NOT NULL,
   `userId` int NOT NULL,
   `amount` int NOT NULL,
-  `fechaInversion` datetime NOT NULL,
-  `fechaRetorno` datetime NOT NULL,
-  `status` text CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL
+  `plazo` int NOT NULL,
+  `fechaInversion` date NOT NULL,
+  `status` varchar(1) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'I'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `inversiones`
+--
+
+INSERT INTO `inversiones` (`id`, `userId`, `amount`, `plazo`, `fechaInversion`, `status`) VALUES
+(444, 4, 200332, 4, '2024-11-01', 'A'),
+(445, 24, 700, 1, '2024-11-28', 'I');
 
 -- --------------------------------------------------------
 
@@ -92,8 +100,8 @@ INSERT INTO `rifasanuales` (`id`, `anio`, `premio`, `descripcion`, `fechaEntrega
 (7, '2031', 'Tsuru Tuneado', 'Tsuru tuneado dportivo nuevo del año 2029.', '2032-01-01', 'Tsuru.jpg'),
 (8, '2032', '100Mil Pesos', 'CienMil pesos Mexicanos en efectivo.', '2033-01-01', 'mil.png'),
 (9, '2033', '500Mil Pesos', 'QuinientosMil pesos Mexicanos en efectivo.', '2034-01-01', 'mil.png'),
-(10, '2034', 'Casa Nueva', 'La casa de tus sueños no necesita una descripcion', '2035-01-01', 'Casa.jpg'),
-(11, '2020', 'pelo', 'pelo de el brian limpio', '2026-01-01', 'mil.png');
+(10, '2032', 'Casa Nuevas', 'Tus sueños no necesita una desc.', '2035-01-05', 'Casa.jpg'),
+(14, '2032', 'chupones de fierro', 'un chupoong aca bien filoosmn', '2017-01-09', 'Xbox.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,10 +123,14 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `correo`, `clave`, `fechareg`, `perfil`) VALUES
-(1, '2darien Alexis Murguia carrillo', '2daniel.armenta1765@alumnos.udg.mx', 'scrypt:32768:8:1$arlH8o6wkmzMaZqh$e36a470a8d80b41c66143069d24737205b1254c3fa9b8859eaa10b0774f4c3681b2177d2c2035ed47238d3544d02c55288b3f20764122005d0f0977113ce9622', '2024-10-30 12:16:26', 'U'),
-(2, 'darien Alexis Murguia carrillo', 'keit@gmail.com', 'scrypt:32768:8:1$kJmPTiQGUYXgCg5l$019409d12667762774bf047e275e483bdeb1e1f0e3f3a0c6de89c6d088399ecf1bbe377888ab3359031eaef313fd6972b30f51ae37fefdd94df92e8ae868ab63', '2024-10-30 12:16:46', 'U'),
-(3, 'Kanet Sahid Ochoa Guzmán', 'kanet.ochoa1921@alumnos.udg.mx', 'scrypt:32768:8:1$bXUWDjkp7qnb7Uwc$4956b6058541b4b8e99e034b69423646a8c2a11100dd16be526d4c387cfb9e6d18bcc8f94ad6221fb73a4fd4bd194d7f8dd7342fb0ec7e7d573377cf6ef200c0', '2024-10-30 12:20:57', 'A'),
-(4, '', '', 'scrypt:32768:8:1$juO46JpCWtKlnZNi$94ba3ba9cc3524360b041d6e981b83363832f25165fcbf92e6f5e307dcf7822d51e6f8e386d443ae6cdece06d3598dd58e923a7ee0d6181e4bdfd25f74d1674e', '2024-11-07 10:54:22', 'A');
+(3, 'Kanet Sahid Ochoa Guzmán', 'kanet.ochoa1921@alumnos.udg.mx', 'scrypt:32768:8:1$jUVM8uz0JwrXfxbc$6a7fc0e8ca07e7e025da56692110269ae892ca489ffea63c99c1d45b5fecf7ac734e01b5337dddf0e45e33bdaa0808279d97c5f167763acde7dec087d857b453', '2024-11-25 18:17:03', 'A'),
+(4, '', '', 'scrypt:32768:8:1$plKxNF9awDUC6WLm$50343bae1dc630bbf6134d5e7f8b4537b5f9f427c8c346e43cbf29b7966bd63caf4e6da6450c163d49595475c09818387f28370607f207badd34cfb19fc2ad18', '2024-11-20 21:20:02', 'A'),
+(6, 'Kanet Sahid', 'tilinesaf@gmail.com', 'scrypt:32768:8:1$UslMP2Eg5KOStIbG$1fc93c0f748d984facfb66ae8330de9fe60f5636f4cabfa8654f000dea5a87b230d071163496590c04590051679c7dbc1ee2b6cb723a1a6000be74ec3612a31b', '2024-11-21 19:13:22', 'U'),
+(8, 'Kanet Sahid', 'tilinesaddf@gmail.com', 'scrypt:32768:8:1$18ugITcyeU4MIuWL$cab13c37be6bdbb86f8a042f4bf303a4b83a0ffe4359af103446891eb00830295ae3619b41a8deb3a1acad9fd68eb041473fa9c88c6dbc31765a450119c67601', '2024-11-21 19:14:13', 'U'),
+(10, 'Kanet Sahid', 'tilinesadafdf@gmail.com', 'scrypt:32768:8:1$PJkxwjLCFtPqMolH$d136e026790f2e8eae90eb9eb3e5b573c30d7d17c7a00748bb0ff821553dc09e549da691613ae83cb91dbd8024a4b55d3368723ee124db997f16aebcb24f3564', '2024-11-21 19:16:16', 'U'),
+(15, 'knet', 'kanet.ochoa19221@alumnos.udg.mx', 'scrypt:32768:8:1$4zN1Vaqvw75fALHA$af7b8b18f040ad979383fdcbb0ad63518624ab90000fb8731770ede35b988e3ec55fa4046bd350e2b3541460c9c68b6789ed5c54ca49938904f7d5bb8d3b8756', '2024-11-25 18:11:45', 'U'),
+(17, 'pipi', 'angelasilva@gmail.com', 'scrypt:32768:8:1$8iuW1jwYTwFAzbtj$12e25564c6fed6cf5d13451530a1ca33be70bfb4299a25623ac631d033b2be44c881e7c7c6c521aa958ba3c60940113df23e54fe585cc19b088004873925a16b', '2024-11-25 18:16:33', 'U'),
+(24, 'Kanet Sahidssss', 'kanetsahid@gmail.com', 'scrypt:32768:8:1$j4Hn1VUeu1aFdO0R$ce998f3a326097fcc66941ae354537c3cb52d47aeb6f7ffaf7a7be3826694f0febcda07bc579df23947403a4fc66daa2d6a5f2ab8c4a936cd8ea18a57f444e43', '2024-11-28 21:36:29', 'U');
 
 --
 -- Índices para tablas volcadas
@@ -166,7 +178,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `inversiones`
 --
 ALTER TABLE `inversiones`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=446;
 
 --
 -- AUTO_INCREMENT de la tabla `opcionesdeinversion`
@@ -184,13 +196,13 @@ ALTER TABLE `retornos`
 -- AUTO_INCREMENT de la tabla `rifasanuales`
 --
 ALTER TABLE `rifasanuales`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Restricciones para tablas volcadas
